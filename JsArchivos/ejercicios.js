a959af41c2cef2dbaa2ee3c5798e9ce08a569476
LiffaOnE <feliz.fuentes67@gmail.com>
2023-03-08 04:27:28 -0300

    primer version

---
+++ JsArchivos/ejercicios.js
@@ -0,0 +1,60 @@
+// let miApellido = "Fuentes";
+// let miNombre = 'Felipe';
+// let miEdad = 19;
+// let miMascota = 'Tomy';
+// let edadMascota = 5;
+
+// const nombreCompleto = miApellido+ ' ' + miNombre;
+
+// const textoPresentacion = 'estas son todas las variables' + ' ' + miApellido + ' ' + miNombre + ' '  + miEdad + ' '  + miMascota + ' '  + edadMascota;
+
+// const SumaEdades =miEdad + edadMascota;
+// const restaEdades = miEdad - edadMascota;
+// const productoEdades = miEdad * edadMascota;
+// const divisionEdades = miEdad / edadMascota;
+
+// // const confirmacionPromt = prompt('el nombre ingresado es: ' +miNombre) // no entendí estos dos ej
+
+// const alumno = {
+//     nombre: 'Raulito',
+//     apellido: 'cordial',
+//     curso: '5b',
+//     edad:14,
+//     promedio: 8
+// }
+// const mascota = {
+//     nombre: 'Tom',
+//     edad:14,
+//     raza:'solo Dios sabe',
+//     color:'marron',
+//     cariñoso: true
+// }
+
+// const frutas = ['banana','manzana','pera','sandia','frutilla'];
+// const numeros = [1,2,3,4,5];
+// const familia = ['felipe','paulo','chiara','agu','dani']
+
+// console.log(miApellido,miNombre,miEdad,miMascota,edadMascota);
+// console.log(nombreCompleto);
+// console.log(textoPresentacion);
+// console.log(SumaEdades,'edades sumadas');
+// console.log(restaEdades,'edades restadas');
+// console.log(productoEdades,'productoEdades');
+// console.log(divisionEdades,'división de las edades')
+
+// console.table(alumno);
+// console.log(Object.entries(alumno));
+// console.table(mascota);
+// console.log(Object.entries(mascota));
+// console.log(frutas);
+// frutas.forEach(function (index){
+//     console.log(index)
+// });
+// console.log(numeros);
+// numeros.forEach(function (index){
+//     console.log(index)
+// });
+// console.log(familia);
+// familia.forEach(function (index){
+//     console.log(index)
+// });
---
+++ JsArchivos/home.js
@@ -0,0 +1,83 @@
+// function fechaBase(fechaInicio,fechaActual){
+//     return fechaInicio + fechaActual
+// }
+// console.log(fechaBase)
+
+// const fechaActual = (fechaActual) => {
+//     return data.events[date]
+// }
+
+// console.log(fechaActual)
+
+// const fechaInicio = (fechaUno) => {
+//     return data.currentDate;
+// }
+// console.log(fechaInicio)
+// data.events.forEach(function (index) {
+//     console.log(index)
+// });
+
+let contenedor = document.getElementById("contenedor")
+
+// for (let i = 0; i < data.events.length; i++) {
+
+//     let div = document.createElement("div")
+//     let parrafo = document.createElement("p")
+//     parrafo.innerText = data.events[i]
+//     div.appendChild(parrafo)
+//     console.log(data.events)
+
+//     contenedor.appendChild(div)
+
+// }
+
+let showCardContainer = document.getElementById('showCards')
+console.log(showCardContainer)
+let stringHtml = ""
+
+for (events of data.events) {
+    stringHtml += `
+    <div class="card" id="card1">
+    <figure>
+        <img src="${events.image}" alt="">
+    </figure>
+    <div class="contenido">
+        <h3>${events.name}</h3>
+        <p>${events.description}</p>
+        <p>Price: $${events.price}<a href="">Load More</a></p>
+    </div>
+    </div>`
+}
+showCardContainer.innerHTML += stringHtml
+
+
+// let showCard = document.getElementById('card1');
+
+// showCard.classList += ' blue'
+// showCard.innerHTML = "<h2> Contenido principal</h2><p>Terrible parrafo papaaa</p>"
+
+// console.log(showCard)
+
+
+// let card = document.createElement('div')
+    // card.classList.add("card")
+    // card.style.width = "18rem"
+
+    // let imagen = document.createElement("img")
+
+    // imagen.src = "/assets/img/Marathon.jpg"
+
+    // let div = document.createElement("div")
+
+    // div.innerHTML = `<div class="card">
+    // <div class="contenido">
+    //      <h3>${events.name}</h3>
+    //      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+    //      <p>Price: $10000 <a href="">Load More</a></p>
+    //  </div>`
+
+
+    // card.appendChild(imagen)
+    // card.appendChild(div)
+    
+    // showCardContainer.appendChild(card)
